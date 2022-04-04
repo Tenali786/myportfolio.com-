13b659b9bd198649552bc71e6413b4d6fb13a1da
Tenali Ramkrishna <89650675+Tenali786@users.noreply.github.com>
2022-04-04 14:36:10 +0530

    n

--- Doc/functions.js
+++ Doc/functions.js
@@ -1,22 +1,27 @@
 const show = document.getElementById('nav');
 const list = document.getElementById('mob');
-const nav = document.getElementsByName('nav');
+const nav = document.getElementById('navlist');
 
 let value = 0;
 function showMenu() {
-    if(value==0){
-
-        list.style.transition= 'right 0.3s';
         list.style.display= 'block';
         list.style.right= '0px';
-        list.appendChild(show);        
         list.style.display = 'block';
+        list.appendChild(show);   
+        // list.style.visibility = "visible";
 
-    }
 }
 
 function CloseList(){
-    list.style.right = '-450px';
-    list.style.display = 'none';
+    list.style.right = '960px';
+    // list.style.visibility = "hidden";
+}
 
+function showAccount(){
+    document.getElementById('closenow').style.left= '0px';
+    document.getElementById('closenow').style.display= 'block';
+}
+
+function closeaccount(){
+    document.getElementById('closenow').style.left= '-300px'
 }
